a9ad2c7356cdc36a9c4647a59a97085af51b05fa
Sudharani3330 <82804527+Sudharani3330@users.noreply.github.com>
2021-05-26 23:42:27 +0530

    Add files via upload

---
+++ testcase.py
@@ -0,0 +1,20 @@
+# 1.
+import shutil
+
+# new_file = open('f1.txt', 'x')
+
+# 2.
+import os
+
+# print(os.path.isfile('f1.txt'))
+
+# 3.
+new_file1 = open('f3.txt', 'w')
+data = new_file1.write('this is a write file')
+print(data)
+
+# 4.
+new_file1 = open('f3.txt', 'r')
+# shutil.move(r'C:\Users\sudha\venv',r'C:\Users\sudha\PycharmProjects\datatypes')
+print(os.getcwd())
+
