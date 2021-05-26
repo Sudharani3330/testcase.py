852b5738db5b9e987380ef759b2d27fd85f28852
Sudharani3330 <82804527+Sudharani3330@users.noreply.github.com>
2021-05-26 23:44:53 +0530

    Update testcase.py

--- testcase.py
+++ testcase.py
@@ -1,11 +1,9 @@
 # 1.
 import shutil
-
 # new_file = open('f1.txt', 'x')
 
 # 2.
 import os
-
 # print(os.path.isfile('f1.txt'))
 
 # 3.
@@ -17,4 +15,6 @@ print(data)
 new_file1 = open('f3.txt', 'r')
 # shutil.move(r'C:\Users\sudha\venv',r'C:\Users\sudha\PycharmProjects\datatypes')
 print(os.getcwd())
+# 5.
+print(os.path.isfile(r'C:\Users\sudha\Desktop\new.txt'))
 
